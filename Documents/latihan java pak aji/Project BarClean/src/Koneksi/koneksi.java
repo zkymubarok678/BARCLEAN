16c5a9c379a3a4ea8387a166636eb83e254cf321
AhmadAkmalFauzan <ojanfauzan81107@gmail.com>
2025-01-15 13:16:51 +0700

    koneksi

---
+++ Documents/latihan java pak aji/Project BarClean/src/Koneksi/koneksi.java
@@ -0,0 +1,28 @@
+package Koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi{
+   private static Connection connection = null;
+   public static Connection getKoneksi(){
+       if(connection != null){
+           return connection;
+       }
+       else{
+           String dbUrl = "jdbc:mysql://localhost:3306/barclean";
+           try{
+               Class.forName("com.mysql.jdbc.Driver");
+               connection = DriverManager.getConnection(dbUrl, "root", "");
+               System.out.println("Koneksi Sukses");
+           }
+           catch(ClassNotFoundException | SQLException e){
+               System.out.println("Koneksi Gagal: " + e);
+           }
+           return connection;
+       } 
+   }
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+}
