983225d2b2f6c36934d9d1e7ceaa5388bca48786
AhmadAkmalFauzan <ojanfauzan81107@gmail.com>
2025-01-20 14:04:51 +0700

    Update Project Java

---
+++ Documents/latihan java pak aji/Project BarClean/src/Model/Pelanggan.java
@@ -0,0 +1,38 @@
+
+package Model;
+public class Pelanggan {
+    private String id_pelanggan,nama,alamat,telepon;
+
+    public String getId_pelanggan() {
+        return id_pelanggan;
+    }
+
+    public void setId_pelanggan(String id_pelanggan) {
+        this.id_pelanggan = id_pelanggan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+    
+}
